37375556be1d030ed8e7855aa55547a4d6ae57ca
SVA2021 <sofronov.vit@gmail.com>
2022-01-26 22:46:56 +0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//******* header functionality ************************//
+// menu hamburger button expand/collapse
+const menuBtn = document.querySelector('.menu__icon');
+const menu = document.querySelector('.header__container');
+const itemExpandedArray = document.querySelectorAll('.item__expand');
+
+menuBtn.addEventListener('click', () => {
+	menu.classList.toggle('active');
+}
+);
+
+for (const item of itemExpandedArray) {
+	item.addEventListener('click', (event) => {
+		event.preventDefault();
+		let hiddenList = item.nextElementSibling;
+		hiddenList.classList.toggle('item__active');
+	})
+}
