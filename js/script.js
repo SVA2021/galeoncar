c9bdb52751c00fc935c7d97a7885e71f84f1c08b
SVA2021 <sofronov.vit@gmail.com>
2022-01-27 21:32:23 +0400

    header mobile first finished

--- js/script.js
+++ js/script.js
@@ -2,22 +2,23 @@
 
 //******* header functionality ************************//
 // menu hamburger button expand/collapse
-const menuBtn = document.querySelector('.menu__icon');
-const menu = document.querySelector('.header__container');
-const itemExpandedArray = document.querySelectorAll('.item__expand');
+const menuBtn = document.querySelector('.header__menu-bar');
+const appShop = document.querySelector('.header__app-shop');
+const menuList = document.querySelector('.menu__body');
 
 menuBtn.addEventListener('click', () => {
-	menu.classList.toggle('active');
+	appShop.classList.toggle('app-shop__active');
+	menuList.classList.toggle('menu__active');
 }
 );
 
-for (const item of itemExpandedArray) {
-	item.addEventListener('click', (event) => {
-		event.preventDefault();
-		let hiddenList = item.nextElementSibling;
-		hiddenList.classList.toggle('item__active');
-	})
-}
+// for (const item of itemExpandedArray) {
+// 	item.addEventListener('click', (event) => {
+// 		event.preventDefault();
+// 		let hiddenList = item.nextElementSibling;
+// 		hiddenList.classList.toggle('item__active');
+// 	})
+// }
 
-let result = document.querySelector('.main').style.font;
-console.log(result);
+// let result = document.querySelector('.main').style.font;
+// console.log(result);
