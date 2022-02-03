835ab080aa5094f0185a9d3ee4c48d58f78f0a3c
SVA2021 <sofronov.vit@gmail.com>
2022-02-03 21:43:45 +0400

    contacts finished

--- js/script.js
+++ js/script.js
@@ -259,3 +259,17 @@ function setSlider(sliderWrapper) {
 }
 
 setSlider(bannerSliderWrapper);
+
+//******* contacts hide menu functionality ************************//
+
+const contactsBlock = document.querySelector('.contacts__inner');
+const contactsOpen = document.querySelector('.contacts__open');
+const contactsClose = document.querySelector('.contacts__close');
+
+contactsClose.addEventListener('click', function(e) {
+	contactsBlock.style.display = 'none';
+})
+
+contactsOpen.addEventListener('click', function(e) {
+	contactsBlock.style.display = 'block';
+})
