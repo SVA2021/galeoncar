d98c118d12bf9fa30e44896c8c8ae06352c47c6b
SVA2021 <sofronov.vit@gmail.com>
2022-02-07 01:01:32 +0400

    business car page initial content created

---
+++ js/script_header_menu.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//******* header ************************//
+// menu hamburger button expand/collapse
+const menuBtn = document.querySelector('.header__menu-bar');
+const appShop = document.querySelector('.header__app-shop');
+// const menuList = document.querySelector('.menu__body');
+const menuList = document.querySelector('.header__menu');
+
+let windowWidth = document.documentElement.clientWidth;
+let windWidth = document.documentElement.offsetWidth;
+
+menuBtn.addEventListener('click', () => {
+	if (windowWidth <= 767) {
+		appShop.classList.toggle('app-shop__active');
+	}
+	menuList.classList.toggle('menu__active');
+}
+);
