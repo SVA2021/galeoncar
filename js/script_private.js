b44c81fa101cc9839940d9481c984b8222014bc2
SVA2021 <sofronov.vit@gmail.com>
2022-02-23 13:19:25 +0400

    private page initial

---
+++ js/script_private.js
@@ -0,0 +1,33 @@
+'use strict';
+
+//******* header ************************//
+// menu hamburger button expand/collapse
+const menuBtn = document.querySelector('.header__menu-bar');
+const appShop = document.querySelector('.header__app-shop');
+// const menuList = document.querySelector('.menu__body');
+const menuList = document.querySelector('.header__menu');
+
+let windowWidth = document.documentElement.clientWidth;
+let windWidth = document.documentElement.offsetWidth;
+
+menuBtn.addEventListener('click', () => {
+	if (windowWidth <= 767) {
+		appShop.classList.toggle('app-shop__active');
+	}
+	menuList.classList.toggle('menu__active');
+}
+);
+
+//******* main search button for modal window  ************************//
+
+const openSearchModalBtn = document.querySelector('.search__open');
+const closeSearchModalBtn = document.querySelector('.search__close');
+const searchModalWindow = document.querySelector('.search__modal');
+
+openSearchModalBtn.addEventListener('click', () => {
+	searchModalWindow.style.display = 'block';
+})
+
+closeSearchModalBtn.addEventListener('click', () => {
+	searchModalWindow.style.display = 'none';
+})
