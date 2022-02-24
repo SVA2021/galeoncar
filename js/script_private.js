3e52748d9ed6e8fdbeba3fab936b9297a78c6bcd
SVA2021 <sofronov.vit@gmail.com>
2022-02-25 02:57:07 +0400

    private page script added

--- js/script_private.js
+++ js/script_private.js
@@ -1,10 +1,8 @@
 'use strict';
 
 //******* header ************************//
-// menu hamburger button expand/collapse
 const menuBtn = document.querySelector('.header__menu-bar');
 const appShop = document.querySelector('.header__app-shop');
-// const menuList = document.querySelector('.menu__body');
 const menuList = document.querySelector('.header__menu');
 
 let windowWidth = document.documentElement.clientWidth;
@@ -18,16 +16,71 @@ menuBtn.addEventListener('click', () => {
 }
 );
 
-//******* main search button for modal window  ************************//
+//******* form actions  ************************//
+const loginForm = document.querySelector('.login');
+const signupForm = document.querySelector('.signup');
+const restoreForm = document.querySelector('.restore');
 
-const openSearchModalBtn = document.querySelector('.search__open');
-const closeSearchModalBtn = document.querySelector('.search__close');
-const searchModalWindow = document.querySelector('.search__modal');
+const loginFormBtn = signupForm.querySelector('.form__btn-enter');
+const signupFormBtn = loginForm.querySelector('.form__btn-reg');
+const forgotLink = document.getElementById('forgot');
+const authLink = document.getElementById('auth');
 
-openSearchModalBtn.addEventListener('click', () => {
-	searchModalWindow.style.display = 'block';
+loginFormBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+	signupForm.classList.remove('form__active');
+	loginForm.classList.add('form__active');
 })
 
-closeSearchModalBtn.addEventListener('click', () => {
-	searchModalWindow.style.display = 'none';
+forgotLink.addEventListener('click', function(e) {
+	e.preventDefault();
+	loginForm.classList.remove('form__active');
+	restoreForm.classList.add('form__active');
 })
+
+signupFormBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+	loginForm.classList.remove('form__active');
+	signupForm.classList.add('form__active');
+})
+
+authLink.addEventListener('click', function(e) {
+	e.preventDefault();
+	restoreForm.classList.remove('form__active');
+	loginForm.classList.add('form__active');
+})
+
+loginForm.classList.add('form__active');
+
+// function setLoginForm(e) {
+// 	e.preventDefault();
+// 	signupForm.classList.remove('form__active');
+// 	restoreForm.classList.remove('form__active');
+// 	loginForm.classList.add('form__active');
+// }
+
+// function setSignupForm(e) {
+// 	e.preventDefault();
+// 	loginForm.classList.remove('form__active');
+// 	signupForm.classList.add('form__active');
+// }
+
+// function setRestoreForm(e) {
+// 	e.preventDefault();
+// 	loginForm.classList.remove('form__active');
+// 	restoreForm.classList.add('form__active');
+// }
+
+// //******* main search button for modal window  ************************//
+
+// const openSearchModalBtn = document.querySelector('.search__open');
+// const closeSearchModalBtn = document.querySelector('.search__close');
+// const searchModalWindow = document.querySelector('.search__modal');
+
+// openSearchModalBtn.addEventListener('click', () => {
+// 	searchModalWindow.style.display = 'block';
+// })
+
+// closeSearchModalBtn.addEventListener('click', () => {
+// 	searchModalWindow.style.display = 'none';
+// })
