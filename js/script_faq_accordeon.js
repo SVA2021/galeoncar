eb1f638185ed0b1df7bc67d09e21795fd40a45a6
SVA2021 <sofronov.vit@gmail.com>
2022-05-09 09:05:51 +0400

    style: accordeon animation added

--- js/script_faq_accordeon.js
+++ js/script_faq_accordeon.js
@@ -7,12 +7,17 @@ const accordeonItems = accordeon.querySelectorAll('.accordeon__item');
 
 for (const item of accordeonItems) {
 	item.addEventListener('click', function () {
+		let list = item.querySelector('.accordeon__sublist');
+		// let fullHeight = list.scrollHeight + 'px';
 		if (item.classList.contains('accordeon__active')) {
+			list.style.maxHeight = 0;
 			item.classList.remove('accordeon__active');
 		} else {
 			let activeItem = accordeon.querySelector('.accordeon__active');
 			activeItem?.classList.remove('accordeon__active');
 			item.classList.add('accordeon__active');
+			let fullHeight = list.scrollHeight + 'px';
+			list.style.maxHeight = fullHeight;
 		}
 	})
 }
